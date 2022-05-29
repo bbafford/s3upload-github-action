8f17f496560c4d6e836d08a490a41b42204ed044
Brock Bafford <brock.bafford@gmail.com>
2022-05-28 21:50:21 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 aws s3 cp * \
-  s3://${S3_BUCKET} \
+  s3://${AWS_S3_BUCKET} \
   --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
