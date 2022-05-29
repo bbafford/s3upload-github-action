d9a73fc5df0a92768958b46439890bcd79902237
Brock Bafford <brock.bafford@gmail.com>
2022-05-28 21:40:32 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh 
+
+set -e
+
+mkdir -p ~/.aws
+
+touch ~/.aws/credentials
+
+echo "[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+aws s3 cp * \
+  s3://${S3_BUCKET} \
+  --region ${AWS_REGION} $*
+
+rm -rf ~/.aws
